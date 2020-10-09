938d35b12ada5097053bbad8e5b3c5f95cbde0d5
Anna Sidorova <xbitstream@gmail.com>
2020-10-09 22:01:15 +0300

    kotlin version down back

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
-    const val version = "1.5.0"
+    const val version = "1.5.1"
     const val build = 34
-    const val kotlinVersion = "1.4.10"
+    const val kotlinVersion = "1.3.72"
 
     object Plugins {
         const val ktlint = "org.jlleitschuh.gradle:ktlint-gradle:9.3.0"
